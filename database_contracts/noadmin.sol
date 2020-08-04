6da668f814e9f3c85350c68ea978c0412483ae29
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2020-08-04 13:14:21 -0400

    Create noadmin.sol

---
+++ database_contracts/noadmin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+
+//All strings are to be placed as JSON format, to keep the low costs, we can use the gz, bzip2 or deflate to compress.
+//Compressing and decompressing is done externally.
+
+pragma solidity >= 0.5 .0 < 0.8 .0;
+
+
+contract Schema
+{
+	mapping(address => mapping(string => string)) dataArray;
+
+	//--------------Data Read functions----------------------------------------
+	function getDataValue(string memory key) public view returns(string memory data)
+	{
+		return dataArray[msg.sender][key];
+	}
+
+	//--------------Data Write/Update functions--------------------------------
+	function insert(uint id, string memory key, string memory data) public returns(bool success)
+	{
+		dataArray[msg.sender][key] = data;
+		return true;
+	}
+	
+}
