01670a00d94e1d9e86de5f83565e90bd0b3870e8
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2020-08-08 12:17:57 -0400

    Update core.sol

--- token/core.sol
+++ token/core.sol
@@ -1,28 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >= 0.5 .0 < 0.8 .0;
-
-//TODO: make the access to token.sol tables
+pragma solidity = 0.7 .0;
 
 library SafeMath {
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
+  function add(uint256 a, uint256 b) internal pure returns(uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
 
-        return c;
-    }
+    return c;
+  }
 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
+  function sub(uint256 a, uint256 b) internal pure returns(uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
 
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
+  function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns(uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
 
-        return c;
-    }
+    return c;
+  }
 
 }
 
@@ -70,38 +68,52 @@ contract Ownable is Context {
 }
 
 interface IERC20 {
-    
-function currentTokenContract() external view returns(address routerAddress);
+
+  function currentTokenContract() external view returns(address routerAddress);
+
   function currentRouterContract() external view returns(address routerAddress);
+
+  function transfer(address[2] memory addressArr, uint[2] memory uintArr) external returns(bool success);
+
+  function approve(address[2] memory addressArr, uint[2] memory uintArr) external returns(bool success);
+
+  function transferFrom(address[3] memory addressArr, uint[3] memory uintArr) external returns(bool success);
+
+  function increaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) external returns(bool success);
+
+  function decreaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) external returns(bool success);
+
 }
-
-
 
 abstract contract Token {
-function balanceOf(address account) external view virtual returns(uint256 data);
-function allowance(address owner, address spender) external view virtual returns(uint256 data);
-function updateBalance(address user, uint newBalance) external virtual returns(bool success);
-function updateAllowance(address owner, address spender, uint newAllowance) external virtual returns(bool success);
-function updateSupply(uint newSupply) external virtual returns(bool success);
-function emitTransfer(address fromAddress, address toAddress, uint amount) external virtual returns(bool success);
-function emitApproval(address fromAddress, address toAddress, uint amount) external virtual returns(bool success);
-    
-}
+  function balanceOf(address account) external view virtual returns(uint256 data);
 
+  function allowance(address owner, address spender) external view virtual returns(uint256 data);
+
+  function updateBalance(address user, uint newBalance) external virtual returns(bool success);
+
+  function updateAllowance(address owner, address spender, uint newAllowance) external virtual returns(bool success);
+
+  function updateSupply(uint newSupply) external virtual returns(bool success);
+
+  function emitTransfer(address fromAddress, address toAddress, uint amount) external virtual returns(bool success);
+
+  function emitApproval(address fromAddress, address toAddress, uint amount) external virtual returns(bool success);
+
+}
 
 //============================================================================================
 // MAIN CONTRACT 
 //============================================================================================
 
 contract Core is IERC20, Ownable {
-    
-using SafeMath for uint256;
 
+  using SafeMath
+  for uint256;
 
   address public tokenContract;
   address public routerContract;
   Token private token;
-
 
   bool private mainConstructorLocked = false;
 
@@ -114,7 +126,6 @@ using SafeMath for uint256;
     }
   }
 
-
   function currentTokenContract() override external view virtual returns(address tokenAddress) { //view
     return tokenContract;
   }
@@ -123,84 +134,88 @@ using SafeMath for uint256;
     return routerContract;
   }
 
-// //function is not needed if token address is hard-coded in a constructor
-//   function setNewTokenContract(address newTokenAddress) onlyOwner public virtual returns(bool success) { //owner
-//     tokenContract = newTokenAddress;
-//     return true;
-//   }
+  // //function is not needed if token address is hard-coded in a constructor
+  //   function setNewTokenContract(address newTokenAddress) onlyOwner public virtual returns(bool success) { //owner
+  //     tokenContract = newTokenAddress;
+  //     return true;
+  //   }
 
   function setNewRouterContract(address newRouterAddress) onlyOwner public virtual returns(bool success) { //owner
     routerContract = newRouterAddress;
     return true;
   }
 
+  function transfer(address[2] memory addressArr, uint[2] memory uintArr) override external virtual returns(bool success) { //from router
+    require(msg.sender == routerContract);
+    _transfer(addressArr, uintArr);
+    return true;
+  }
 
+  function _transfer(address[2] memory addressArr, uint[2] memory uintArr) private returns(bool success) {
+    address sender = addressArr[0];
+    address recipient = addressArr[1];
 
+    require(sender != address(0));
 
+    uint amount = uintArr[0];
 
-    
-    function transfer(address[2] memory addressArr, uint[2] memory uintArr) public virtual returns (bool success) { //from router
-        require(msg.sender == routerContract);
-        address sender = addressArr[0];
-        address recipient = addressArr[1];
-        
-        require(sender!=address(0));
-        
-        uint amount = uintArr[0];
-        
-        require(amount<=token.balanceOf(sender));
-        
-        token.emitTransfer(sender, recipient, amount);
-        return true;
-    }
-    
-    
-    function transferFrom(address[3] memory addressArr, uint[3] memory uintArr) public virtual returns (bool success) { //from router
-        uint allowance = token.allowance(addressArr[1], addressArr[0]);
-        require(allowance>=uintArr[0]);
-        
-        address[2] memory tmpAddresses1 = [addressArr[1], addressArr[2]];
-        address[2] memory tmpAddresses2 = [addressArr[1], addressArr[0]];
-        
-        uint[2] memory tmpUint = [uintArr[0],uintArr[1]];
+    require(amount <= token.balanceOf(sender));
 
-        approve(tmpAddresses2, tmpUint);
-        transfer(tmpAddresses1, tmpUint);
-       
-        return true;
-    }
-    
-    
-    function approve(address[2] memory addressArr, uint[2] memory uintArr) public virtual returns (bool success){ //from router
-        address owner = addressArr[0];
-        address spender = addressArr[1];
-        uint amount = uintArr[0];
-        
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+    token.emitTransfer(sender, recipient, amount);
+    return true;
+  }
 
-        token.updateAllowance(owner,spender,amount);
-        token.emitApproval(owner,spender,amount);
-            
-        return true;
-    }
- 
-    function increaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) public virtual returns (bool success) { //from router
-        uint newAllowance = token.allowance(addressArr[0],addressArr[1]).add(uintArr[0]);
-        uintArr[0] = newAllowance;
-        approve(addressArr, uintArr);
-        return true;
-    }
-    
-    function decreaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) public virtual returns (bool success) { //from router
-        uint newAllowance = token.allowance(addressArr[0],addressArr[1]).sub(uintArr[0], "ERC20: decreased allowance below zero");
-        uintArr[0] = newAllowance;
-        approve(addressArr, uintArr);
-        return true;
-        
-    }
-    
- 
-    
-    
+  function approve(address[2] memory addressArr, uint[2] memory uintArr) override external returns(bool success) { //from router
+    require(msg.sender == routerContract);
+    _approve(addressArr, uintArr);
+    return true;
+  }
+
+  function _approve(address[2] memory addressArr, uint[2] memory uintArr) private returns(bool success) { //from router
+    address owner = addressArr[0];
+    address spender = addressArr[1];
+    uint amount = uintArr[0];
+
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    token.updateAllowance(owner, spender, amount);
+    token.emitApproval(owner, spender, amount);
+
+    return true;
+  }
+
+  function transferFrom(address[3] memory addressArr, uint[3] memory uintArr) override external virtual returns(bool success) { //from router
+    require(msg.sender == routerContract);
+    uint allowance = token.allowance(addressArr[1], addressArr[0]);
+    require(allowance >= uintArr[0]);
+
+    address[2] memory tmpAddresses1 = [addressArr[1], addressArr[2]];
+    address[2] memory tmpAddresses2 = [addressArr[1], addressArr[0]];
+
+    uint[2] memory tmpUint = [uintArr[0], uintArr[1]];
+
+    _approve(tmpAddresses2, tmpUint);
+    _transfer(tmpAddresses1, tmpUint);
+
+    return true;
+  }
+
+  function increaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) override external virtual returns(bool success) { //from router
+    require(msg.sender == routerContract);
+    uint newAllowance = token.allowance(addressArr[0], addressArr[1]).add(uintArr[0]);
+    uintArr[0] = newAllowance;
+    _approve(addressArr, uintArr);
+    return true;
+  }
+
+  function decreaseAllowance(address[2] memory addressArr, uint[2] memory uintArr) override external virtual returns(bool success) { //from router
+    require(msg.sender == routerContract);
+    uint newAllowance = token.allowance(addressArr[0], addressArr[1]).sub(uintArr[0], "ERC20: decreased allowance below zero");
+    uintArr[0] = newAllowance;
+    _approve(addressArr, uintArr);
+    return true;
+
+  }
+
 }
