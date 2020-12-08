a65056cd5043a5a87dfa0debfc523f4df4358c1b
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2020-12-07 22:16:47 -0500

    Update solo_miner_v2.sol

--- miner/solo_miner_v2.sol
+++ miner/solo_miner_v2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7 .0;
+pragma solidity ^ 0.7 .0;
 
 library SafeMath {
   function add(uint256 a, uint256 b) internal pure returns(uint256) {
@@ -97,11 +97,12 @@ contract Ownable is Context {
   }
 }
 
-
 abstract contract OldVersionMiner {
-    function getPivot() external view virtual returns(uint lastPivot);
-    function getAddressFromId(uint id) external view virtual returns(address minerAddress);
-    function showReward(address minerAddress) public view virtual returns(uint reward);
+  function getPivot() external view virtual returns(uint lastPivot);
+
+  function getAddressFromId(uint id) external view virtual returns(address minerAddress);
+
+  function showReward(address minerAddress) public view virtual returns(uint reward);
 }
 
 abstract contract Token {
@@ -124,41 +125,42 @@ contract SoloMiner is Ownable {
   using SafeMath
   for uint;
 
-  address private tokenContract;
-  address private routerContract;
-
   Token private token;
   Router private router;
   OldVersionMiner private oldVersionMiner;
-  
+
+  //Contract addresses
+  address private tokenContract;
+  address private routerContract;
+
+  //Global
+  uint private rewardConstant = 100000000000000000000;
+  uint private difficultyConstant = 69444444444444; //starts with 1% earning per day, decreases to 190258751902 in 20 years, 1317612 per block
+  uint private decreaseDifficultyConstant = 1317621; // decreases countdownConstant per block
+  uint private mintDecreaseConstant = 500000; //decreases countdownConstant per token mint function
+  uint private creationBlock = 0;
+
+  //Miner specific
+  uint private pivot = 0;
   mapping(address => uint) private userBlocks;
   mapping(address => uint) private miners;
   mapping(uint => address) private addressFromId;
   mapping(address => uint) private depositedTokens;
   mapping(address => uint) private userDifficultyConstant;
 
-
-  uint private pivot = 0;
-  uint private rewardConstant = 100000000000000000000;
-  uint private difficultyConstant = 69444444444444; //starts with 1% earning per day, decreases to 190258751902 in 20 years, 1317612 per block
-  uint private decreaseDifficultyConstant = 1317612; // decreases countdownConstant per block
-  uint private mintDecreaseConstant = 50000; //decreases countdownConstant per token mint function
-  uint private creationBlock = 0;
-
-  
   constructor() {
     oldVersionMiner = OldVersionMiner(address(0x8658299fD312BfFD5F1aF515A031BE93ACe3BF88)); //TODO, CHANGE THIS BEFORE DEPLOY!
     uint oldPivot = oldVersionMiner.getPivot();
     uint currentBlockNumber = getCurrentBlockNumber();
     creationBlock = currentBlockNumber;
-    for(uint i=1;i<=oldPivot;i++){
-        address oldAddress = oldVersionMiner.getAddressFromId(i);
-        uint tokens = oldVersionMiner.showReward(oldAddress);
+    for (uint i = 1; i <= oldPivot; i++) {
+      address oldAddress = oldVersionMiner.getAddressFromId(i);
+      uint tokens = oldVersionMiner.showReward(oldAddress);
 
-        miners[oldAddress] = i;
-        addressFromId[i] = oldAddress;
-        depositedTokens[oldAddress] = tokens;
-        userBlocks[oldAddress] = currentBlockNumber;
+      miners[oldAddress] = i;
+      addressFromId[i] = oldAddress;
+      depositedTokens[oldAddress] = tokens;
+      userBlocks[oldAddress] = currentBlockNumber;
     }
     pivot = oldPivot;
 
@@ -166,59 +168,86 @@ contract SoloMiner is Ownable {
 
   //+++++++++++VIEWS++++++++++++++++
   //----------GETTERS---------------
-  
-  function getPivot() external view virtual returns(uint lastPivot) {
-    return pivot;
-  }
 
-  function getAddressFromId(uint id) external view virtual returns(address minerAddress) {
-    return addressFromId[id];
+  //Contract addresses
+  function getRouterContract() external view virtual returns(address routerAddress) {
+    return routerContract;
   }
-
-  function getUserBlocks(address minerAddress) external view virtual returns(uint minerBlocks) {
-    return userBlocks[minerAddress];
-  }
-
 
   function getTokenContract() external view virtual returns(address tokenAddress) {
     return tokenContract;
+  }
+
+  //Global
+  function getRewardConstant() external view virtual returns(uint returnConstant) {
+    return rewardConstant;
+  }
+
+  function getDifficultyConstant() public view returns(uint256 returnConstant) {
+    return difficultyConstant;
+  }
+
+  function getDecreaseDifficultyConstant() public view returns(uint256 returnConstant) {
+    return decreaseDifficultyConstant;
+  }
+
+  function getMintDecreaseConstant() public view returns(uint256 returnConstant) {
+    return mintDecreaseConstant;
+  }
+
+  function getCreationBlock() public view returns(uint256 blockNumber) {
+    return creationBlock;
+  }
+
+  //Miner specific
+  function getPivot() external view virtual returns(uint lastPivot) {
+    return pivot;
   }
 
   function getLastBlockNumber(address minerAddress) public view virtual returns(uint lastBlock) {
     return userBlocks[minerAddress];
   }
 
-  function getRouterContract() external view virtual returns(address routerAddress) {
-    return routerContract;
+  function getIdFromAddress(address minerAddress) public view returns(uint256 id) {
+    return miners[minerAddress];
   }
 
+  function getAddressFromId(uint id) external view virtual returns(address minerAddress) {
+    return addressFromId[id];
+  }
+
+  function getDepositedTokens(address minerAddress) public view returns(uint256 tokens) {
+    return depositedTokens[minerAddress];
+  }
+
+  function getUserDifficultyConstant(address minerAddress) public view returns(uint256 returnConstant) {
+    return userDifficultyConstant[minerAddress];
+  }
+
+  //Other
   function getCurrentBlockNumber() public view returns(uint256 blockNumber) {
     return block.number;
   }
- 
-  function getRewardConstant() external view virtual returns(uint routerAddress) {
-    return rewardConstant;
-  }
 
-
-  //----------OTHER VIEWS---------------
-
-  function showEarned(address minerAddress) public view virtual returns(uint reward) {
+  function showEarned(address minerAddress) public view virtual returns(uint tokensEarned) {
     uint previousBlock = getLastBlockNumber(minerAddress);
     uint currentBlock = getCurrentBlockNumber();
+    require(previousBlock <= currentBlock, "solo_miner:showEarned:bad block numbers");
     uint diff = currentBlock.sub(previousBlock);
     uint deposited = depositedTokens[minerAddress];
-    
-    if(rewardConstant==0){return 0;}
+
+    if (rewardConstant == 0) {
+      return 0;
+    }
     uint earned = ((deposited.mul(diff)).mul(userDifficultyConstant[minerAddress])).div(rewardConstant);
     return earned;
-    
+
   }
 
   function showReward(address minerAddress) public view virtual returns(uint reward) {
-      uint earned = showEarned(minerAddress);
-      uint ret = depositedTokens[minerAddress].add(earned);
-      return ret;
+    uint earned = showEarned(minerAddress);
+    uint ret = depositedTokens[minerAddress].add(earned);
+    return ret;
   }
 
   //+++++++++++EXTERNAL++++++++++++++++
@@ -230,7 +259,7 @@ contract SoloMiner is Ownable {
     uint deposit = reward.add(depositAmount);
 
     burn(depositAmount);
-    
+
     depositedTokens[msg.sender] = deposit;
     userBlocks[msg.sender] = getCurrentBlockNumber();
     updateDifficulty(msg.sender);
@@ -240,53 +269,66 @@ contract SoloMiner is Ownable {
 
   function getReward(uint withdrawAmount) public virtual returns(bool success) {
     require(getLastBlockNumber(msg.sender) > 0, "solo_miner:getReward:Must mine first");
+    require(mintDecreaseConstant <= difficultyConstant, "solo_miner:getReward:difficulty constants error");
 
     uint reward = showReward(msg.sender);
     require(withdrawAmount <= reward, "solo_miner:getReward:Amount too big");
     registerMiner();
-    
+
     uint balance = reward.sub(withdrawAmount);
-    
+
     depositedTokens[msg.sender] = balance;
     userBlocks[msg.sender] = getCurrentBlockNumber();
 
     mint(withdrawAmount);
+    difficultyConstant = difficultyConstant.sub(mintDecreaseConstant);
     updateDifficulty(msg.sender);
-    
+
     return true;
   }
 
-  function getFullReward() public virtual returns(bool success) {
+  function withdrawAll() public virtual returns(bool success) {
     uint amt = showReward(msg.sender);
     getReward(amt);
     return true;
   }
 
-  //in case you want to burn tokens and increase miner rewards
-  function burnMyTokens(uint tokenAmount) public virtual returns(bool success) {
-    burn(tokenAmount);
-    return true;
-  }
-
   //+++++++++++ONLY OWNER++++++++++++++++
   //----------SETTERS--------------------
+
+  //Contract addresses
   function setNewTokenContract(address newTokenAddress) onlyOwner public virtual returns(bool success) {
     tokenContract = newTokenAddress;
     token = Token(newTokenAddress);
     return true;
   }
-  
+
   function setNewRouterContract(address newRouterAddress) onlyOwner public virtual returns(bool success) {
     routerContract = newRouterAddress;
     router = Router(newRouterAddress);
     return true;
   }
 
+  //Global
   function setRewardConstant(uint newConstant) onlyOwner public virtual returns(bool success) {
     rewardConstant = newConstant;
     return true;
   }
 
+  function setDifficultyConstant(uint newConstant) onlyOwner public virtual returns(bool success) {
+    difficultyConstant = newConstant;
+    return true;
+  }
+
+  function setDecreaseDifficultyConstant(uint newConstant) onlyOwner public virtual returns(bool success) {
+    decreaseDifficultyConstant = newConstant;
+    return true;
+  }
+
+  function setMintDecreaseConstant(uint newConstant) onlyOwner public virtual returns(bool success) {
+    mintDecreaseConstant = newConstant;
+    return true;
+  }
 
   //+++++++++++PRIVATE++++++++++++++++++++   
   function registerMiner() private {
@@ -296,14 +338,17 @@ contract SoloMiner is Ownable {
       addressFromId[pivot] = msg.sender;
     }
   }
-  
+
   function updateDifficulty(address minerAddress) private {
     uint currentBlock = getCurrentBlockNumber();
+    require(creationBlock <= currentBlock, "solo_miner:updateDifficulty:bad block numbers");
     uint diff = currentBlock.sub(creationBlock);
     uint decreaseBy = decreaseDifficultyConstant.mul(diff);
-    if(decreaseBy>difficultyConstant){userDifficultyConstant[minerAddress] = 1;}
-    else{
-    userDifficultyConstant[minerAddress] = difficultyConstant.sub(decreaseBy);
+
+    if (decreaseBy > difficultyConstant) {
+      userDifficultyConstant[minerAddress] = 1;
+    } else {
+      userDifficultyConstant[minerAddress] = difficultyConstant.sub(decreaseBy);
     }
   }
 
@@ -314,17 +359,17 @@ contract SoloMiner is Ownable {
     address[2] memory addresseArr = [msg.sender, toAddress];
     uint[2] memory uintArr = [burnAmount, 0];
 
-    router.extrenalRouterCall("burn", addresseArr, uintArr); //TODO "burn_miner"
+    router.extrenalRouterCall("burn_miner", addresseArr, uintArr);
 
     return true;
   }
-  
+
   function mint(uint mintAmount) private returns(bool success) {
     address fromAddress = address(0);
     address[2] memory addresseArr = [fromAddress, msg.sender];
     uint[2] memory uintArr = [mintAmount, 0];
 
-    router.extrenalRouterCall("mint", addresseArr, uintArr); //TODO "mint_miner"
+    router.extrenalRouterCall("mint_miner", addresseArr, uintArr);
 
     return true;
   }
